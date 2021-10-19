676f339b61e272249b00d07e6b041684f53f94de
merryt <tymerry@gmail.com>
2021-10-19 16:36:48 -0500

    skeloton for sockets

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # acquire
 A digital version of the board game acquire https://boardgamegeek.com/boardgame/5/acquire
+
+
+To get dev setup
+- Clone project
+- Install node (I am using 14.17.6)
+- Install nodemon globally
+- `npm run dev`
+
+
+## Todo
+### Major Areas
+- Login/Auth
+- Chat/Lobby
+- Single Game
+
+### Dev Env
+- Install prettier
---
+++ index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+const app = express();
+import http from 'http';
+const server = http.createServer(app);
+import { Server } from "socket.io";
+const io = new Server(server);
+import {default as Chat} from './EventHandlers/Chat.js'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+// setting up normal patterns in esm
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.get('/', (req, res) => {
+    // check to see if player is logged in (for now we might not want to mess with that)
+
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+
+    // this organisation pattern is taken from https://stackoverflow.com/questions/20466129/how-to-organize-socket-handling-in-node-js-and-socket-io-app
+    var eventHandlers = {
+        chat: new Chat(io, app, socket)
+    };
+
+    for (var category in eventHandlers) {
+        var handler = eventHandlers[category].handler;
+        for (var event in handler) {
+            socket.on(event, handler[event]);
+        }
+    }
+});
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+});
